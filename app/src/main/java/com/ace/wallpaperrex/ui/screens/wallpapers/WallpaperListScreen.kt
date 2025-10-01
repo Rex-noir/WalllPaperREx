ce8a89453407e92e246f5715b66e6575538c8092
acerex <lala@lailen.com>
2025-10-01 15:25:24 +0530

    Refactor: Update imports in WallpaperListScreen.kt
    - Adds `androidx.compose.ui.res.painterResource` and `coil3.compose.rememberAsyncImagePainter` imports.
    - Adds import for `com.ace.wallpaperrex.R`.

--- app/src/main/java/com/ace/wallpaperrex/ui/screens/wallpapers/WallpaperListScreen.kt
+++ app/src/main/java/com/ace/wallpaperrex/ui/screens/wallpapers/WallpaperListScreen.kt
@@ -29,12 +29,15 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
+import coil3.compose.rememberAsyncImagePainter
 import coil3.request.ImageRequest
 import coil3.request.crossfade
+import com.ace.wallpaperrex.R
 import com.ace.wallpaperrex.data.ImageItem
 
 @Composable
