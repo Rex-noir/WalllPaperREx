e394bb2b500c1ca15f35c9f8de33028e4576acbc
acerex <lala@lailen.com>
2025-10-01 15:37:48 +0530

    Refactor: Relocate `GridImageItem` to components package
    Moves the `GridImageItem` composable from `WallpaperListScreen.kt` to a new file `GridListItem.kt` within the `com.ace.wallpaperrex.ui.components.wallpaper` package.
    
    Updates the import for `GridImageItem` in `WallpaperListScreen.kt` to reflect its new location.

--- app/src/main/java/com/ace/wallpaperrex/ui/screens/wallpapers/WallpaperListScreen.kt
+++ app/src/main/java/com/ace/wallpaperrex/ui/screens/wallpapers/WallpaperListScreen.kt
@@ -16,29 +16,23 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil3.compose.AsyncImage
-import coil3.compose.rememberAsyncImagePainter
 import coil3.request.ImageRequest
 import coil3.request.crossfade
-import com.ace.wallpaperrex.R
 import com.ace.wallpaperrex.data.ImageItem
+import com.ace.wallpaperrex.ui.components.wallpaper.GridImageItem
 
 @Composable
 fun WallpaperListScreen(
@@ -144,20 +138,6 @@ fun WallpaperStaggeredGrid(
     }
 }
 
-@Composable
-fun GridImageItem(item: ImageItem, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    AsyncImage(
-        model = ImageRequest.Builder(LocalContext.current).data(item.urlFull).crossfade(true)
-            .build(),
-        contentDescription = item.description,
-        contentScale = ContentScale.Crop,
-        modifier = modifier
-            .fillMaxWidth()
-            .aspectRatio(item.aspectRatio.takeIf { it > 0 } ?: (3f / 4f))
-            .clip(RoundedCornerShape(size = 0.dp))
-            .clickable(onClick = onClick)
-    )
-}
 
 @Composable
 fun ErrorState(message: String, onRetry: () -> Unit, modifier: Modifier = Modifier) {
