8ab612ff95baf3b52e724c80e94facb6fcfa3aee
acerex <lala@lailen.com>
2025-10-10 15:01:30 +0530

    feat(UI): Enable edge-to-edge display
    This commit enables edge-to-edge display for the application, allowing the UI to draw under the system bars (status and navigation bars) for a more immersive experience.
    
    The change is implemented by calling `WindowCompat.enableEdgeToEdge(window)` in the `MainActivity`'s `onCreate` method.

--- app/src/main/java/com/ace/wallpaperrex/MainActivity.kt
+++ app/src/main/java/com/ace/wallpaperrex/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -39,6 +40,7 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.enableEdgeToEdge(window)
         setContent {
             AppTheme {
                 val appNavController = rememberNavController()
